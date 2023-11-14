8349caa9d427e505d408fe428d97bea70b4c8f8b
ContardiC <c.contardi@icloud.com>
2023-11-14 13:45:41 +0100

    array of drawable added in MainActivity

--- app/src/main/java/it/spacecoding/magicball/MainActivity.java
+++ app/src/main/java/it/spacecoding/magicball/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mArrayBalls[] = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
